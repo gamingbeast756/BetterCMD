94b2db81aabcd9e52ad8cff9df3176f014921dd3
gamingbeast756 <96247536+gamingbeast756@users.noreply.github.com>
2022-05-28 15:49:02 -0400

    replacing

--- BETTERCMD/Recources/asciiart.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /a artnumber=%random% %%3
-type Recources\asciiart\%artnumber%.txt
-@echo.
--- BETTERCMD/Recources/rngen.bat
+++
@@ -1,6 +0,0 @@
-Set /p minimum="What do you want the minimum to be? "
-Set /p maximum="What do you want the maximum to be? "
-Set /a Placeholder=%maximum%-(%minimum%-1)
-Set /a number=%random% %%%Placeholder%
-Set /a number=%number%+%minimum%
-@echo %number%
